5f61e6c14a3a9038658fa23aaeb69d9d307f5043
Bertus8 <albertinisp@gmail.com>
2022-03-09 18:58:27 +0100

    fisrt commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <div className='div-navbar'>
+
+    <NavLink to="/home" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+      <img className='img-navbar' src='./icons/banco.png' alt='casita'/>
+    </NavLink>
+
+    <NavLink to="/about" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+      <img className='img-navbar' src='./icons/pantalla.png' alt='about'/>
+    </NavLink>
+
+    <NavLink to="/projects" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+      <img className='img-navbar' src='./icons/imagen.png' alt='about'/>
+    </NavLink>
+
+    <NavLink to="/contact" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+      <img className='img-navbar' src='./icons/marcador.png' alt='contact'/>
+    </NavLink>
+
+  </div>
+  )
+}
+
+export default Nav
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.scss';
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
